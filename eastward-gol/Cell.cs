101892864886d594e56ef6eb2d35d943512989b1
david-adsit <david-adsit@pluralsight.com>
2012-08-17 00:19:41 -0600

    The one where Bob forgot the rules

--- eastward-gol/Cell.cs
+++ eastward-gol/Cell.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace eastward_gol
 {
     public class Cell
@@ -54,5 +57,35 @@ namespace eastward_gol
         {
             return !Equals(left, right);
         }
+
+        public void AddToWorld(World world, Position position, Neighborhood neighbors)
+        {
+            var nextGeneration = Dead;
+            if (isAlive && (livingNeighbors.Count() == 2 || livingNeighbors.Count() == 3))
+            {
+                nextGeneration = Alive;
+            }
+            else
+            {
+                if (livingNeighbors.Count() == 3)
+                {
+                    nextGeneration = Alive;
+                }
+            }
+            world.SetNextGeneration(position, nextGeneration);
+        }
+
+        public void AddIfLiving(Neighborhood neighbors)
+        {
+            if (isAlive)
+            {
+                neighbors.Add(this);
+            }
+        }
+    }
+
+    public class Neighborhood : List<Cell>
+    {
+        
     }
 }
--- eastward-gol/World.cs
+++ eastward-gol/World.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace eastward_gol
@@ -6,26 +7,75 @@ namespace eastward_gol
     public class World
     {
         Cell[] cells;
+        Cell[] nextGeneration;
+        int? _width;
 
         public World(Cell[] cells)
         {
             this.cells = cells;
+            nextGeneration = cells.Select(x => Cell.Dead).ToArray();
         }
 
         public void Print(IPrinter printer)
         {
-            var width = (int) Math.Sqrt(cells.Length);
             for (var i = 0; i < cells.Length; i++)
             {
-                printer.PrintCell(i/width, i%width, cells[i]);
+                printer.PrintCell(i/Width, i%Width, cells[i]);
             }
+        }
+
+        int Width
+        {
+            get { return (int) (_width.HasValue ? _width.Value : _width = (int) Math.Sqrt(cells.Length)); }
         }
 
         public World Tick()
         {
-            cells = cells.Select(x => Cell.Dead).ToArray();
+            for (var row = 0; row < Width; row++)
+            {
+                for (var column = 0; column < Width; column++)
+                {
+                    var currentCell = cells[row*Width + column];
+                    Neighborhood livingNeighbors = GetLivingNeighbors(row, column);
 
+                    currentCell.AddToWorld(this, new Position(row, column), livingNeighbors);
+                }
+            }
+            MoveGenerations();
             return this;
+        }
+
+        Neighborhood GetLivingNeighbors(int row, int column)
+        {
+            row = row - 1;
+            column = column - 1;
+            var neighbors = new Neighborhood();
+            var cell = cells[row*Width + column];
+            cell.AddIfLiving(neighbors);
+            return neighbors;
+        }
+
+        void MoveGenerations()
+        {
+            cells = nextGeneration;
+            nextGeneration = cells.Select(x => Cell.Dead).ToArray();
+        }
+
+        public void SetNextGeneration(Position position, Cell cell)
+        {
+            nextGeneration[position.Row*Width + position.Column] = cell;
+        }
+    }
+
+    public struct Position
+    {
+        public readonly int Row;
+        public readonly int Column;
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
         }
     }
 }
